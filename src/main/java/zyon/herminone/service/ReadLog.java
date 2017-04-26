7a4390204564f436120b0c0e954cc2fe69595891
Zyon Dias <zyon.dias@hotmail.com>
2017-04-26 19:03:41 -0300

    setando responseCode no objeto LogLine

--- src/main/java/zyon/herminone/service/ReadLog.java
+++ src/main/java/zyon/herminone/service/ReadLog.java
@@ -6,6 +6,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import zyon.herminone.vo.LogLine;
 
@@ -38,6 +40,13 @@ public class ReadLog {
 	
 	private LogLine parseLineToObject(String line){
 		LogLine logLine = new LogLine();
+		
+		Pattern patternResSstatus = Pattern.compile(".*response_status=\"(.*)\"");
+		Matcher matcherResStatus = patternResSstatus.matcher(line);
+		if(matcherResStatus.find()){
+			logLine.setResponseCode(matcherResStatus.group(1));
+		}
+		
 		return logLine;
 	}
 }
--- src/test/java/zyon/herminone/service/ReadLogTest.java
+++ src/test/java/zyon/herminone/service/ReadLogTest.java
@@ -1,19 +1,45 @@
 package zyon.herminone.service;
 
+import java.util.List;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import zyon.herminone.vo.LogLine;
 
 public class ReadLogTest {
 
+	private ReadLog readLog;
+
+	@Before
+	public void init() {
+		this.readLog = new ReadLog();
+	}
+
 	@Test
-	public void deveLerTodasLinhasDeLog(){
+	public void deveLerTodasLinhasDeLog() {
 		// give
 		String path = "src/test/resources/log-teste.txt";
+
 		// when
-		ReadLog readLog = new ReadLog();
 		int result = readLog.execute(path).size();
+
 		// then
 		Assert.assertEquals(2, result);
+
+	}
+
+	@Test
+	public void devePegarOsValoresDeResponseCodeCorretamente() {
+		// give
+		String path = "src/test/resources/log-teste.txt";
+
+		// when
+		List<LogLine> result = readLog.execute(path);
 		
+		// then 
+		Assert.assertEquals("200", result.get(0).getResponseCode());
+		Assert.assertEquals("500", result.get(1).getResponseCode());
 	}
 }
