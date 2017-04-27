40ca0f3b9319e5a253cd8662c66dadfff7b2f9c8
Zyon Dias <zyon.dias@hotmail.com>
2017-04-26 23:53:55 -0300

    Add teste de ordem do manager

--- src/test/java/zyon/herminone/manager/ManagerReportLogsTest.java
+++ src/test/java/zyon/herminone/manager/ManagerReportLogsTest.java
@@ -2,6 +2,7 @@ package zyon.herminone.manager;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -16,26 +17,38 @@ public class ManagerReportLogsTest {
 
 	@InjectMocks
 	private ManagerReportLogs managerReportLogs;
-	
+
 	@Mock
 	private ReadLog readLogMock;
 	@Mock
 	private ResponseCodeCalc responseCodeCalcMock;
 	@Mock
 	private UrlRequestCalc urlRequestCalcMock;
-	
+
 	@Test
-	public void deveChamarOsMetodosDasClassesDeServico(){
-		
+	public void deveChamarOsMetodosDasClassesDeServico() {
+
 		// when
 		managerReportLogs.run();
-		
+
 		// then
 		Mockito.verify(readLogMock, Mockito.times(1)).execute(Mockito.anyString());
 		Mockito.verify(responseCodeCalcMock, Mockito.times(1)).total(Mockito.anyList());
 		Mockito.verify(urlRequestCalcMock, Mockito.times(1)).topThreeUrl(Mockito.anyList());
+
+	}
+
+	@Test
+	public void garanteAOrdemDeChamada() {
+		// when
+		managerReportLogs.run();
 		
+		// then
+		InOrder inOrder = Mockito.inOrder(readLogMock, responseCodeCalcMock, urlRequestCalcMock);
+		
+		inOrder.verify(readLogMock).execute(Mockito.anyString());
+		inOrder.verify(responseCodeCalcMock).total(Mockito.anyList());
+		inOrder.verify(urlRequestCalcMock).topThreeUrl(Mockito.anyList());
 	}
 
 }
-
