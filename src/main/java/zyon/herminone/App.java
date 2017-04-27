ad47eeda1e39e845e322c99e170eff98338200a5
Zyon Dias <zyon.dias@hotmail.com>
2017-04-26 23:59:36 -0300

    Criando classe main

--- src/main/java/zyon/herminone/App.java
+++ src/main/java/zyon/herminone/App.java
@@ -1,13 +1,19 @@
 package zyon.herminone;
 
+import zyon.herminone.manager.ManagerReportLogs;
+import zyon.herminone.service.ReadLog;
+import zyon.herminone.service.ResponseCodeCalc;
+import zyon.herminone.service.UrlRequestCalc;
+
 /**
- * Hello world!
+ * Inicializa as depedencias e inicia o processo
+ * @author zyondias
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+        
+    	new ManagerReportLogs(new ReadLog(), new ResponseCodeCalc(), new UrlRequestCalc()).run();;
     }
 }
